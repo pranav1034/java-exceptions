29be794cda2b02da7888956b98d8974ea528afce
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-30 19:21:48 +0530

    [Pranav] ADD handle-IOexception-if-file-does-not-exist

--- src/main/java/com/bridgelabz/javaexceptions/CheckedException.java
+++ src/main/java/com/bridgelabz/javaexceptions/CheckedException.java
@@ -1,4 +1,21 @@
 package com.bridgelabz.javaexceptions;
+import java.io.*;
 
 public class CheckedException {
+    public static void main(String[] args) {
+        try{
+            FileInputStream fis = new FileInputStream("data.txt");
+            FileOutputStream fos = new FileOutputStream("output.txt");
+            int byteData;
+            while((byteData = fis.read())!= -1){
+                fos.write(byteData);
+                System.out.print((char)byteData);
+            }
+        }
+        catch(IOException e){
+            System.out.println("File not found");
+        }
+    }
 }
+
+
